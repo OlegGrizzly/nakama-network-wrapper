63700261eede808277790850ce15b7dc75bd8629
OlegGrizzly <xew@list.ru>
2025-08-10 16:28:19 +0300

    Fix .meta files

--- export-unitypackage.sh
+++ export-unitypackage.sh
@@ -13,3 +13,5 @@ rm Packages/packages-lock.json.meta
 
 # reimport with Unity to clean up any stale import data e.g., EditorBuildSettings.asset
 $1 -batchmode -quit -nographics -projectPath .
+
+# ./export-unitypackage.sh "/Applications/Unity/Hub/Editor/2022.3.62f1/Unity.app/Contents/MacOS/Unity"
